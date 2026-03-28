f2913c1ad4537c8164e4c5f7fca4b83c7ac3d81c
Labasse <labasse@gmail.com>
2026-03-28 18:22:30 +0000

    Better way to deal with first build

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Install Emscripten SDK
 cd /workspaces
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 ./emsdk install latest
 ./emsdk activate latest
-
-# First build to have wasm
-cd $CODESPACE_VSCODE_FOLDER
-bash build.sh . 3000
